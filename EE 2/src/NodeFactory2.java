20e715f5112e97fc2af113ad361d1fc8f091a769
Oliver <Oliver@DESKTOP-TA5MOHC>
2019-08-27 23:58:42 +0300

    Minor changes.

--- EE 2/src/NodeFactory2.java
+++ EE 2/src/NodeFactory2.java
@@ -167,7 +167,7 @@ public class NodeFactory2<Item>{
 		
 		if(pivot.parent != null) {
 			pivot.parent = parent.parent;
-			if(parent.parent != null) { 
+			if(parent.parent != null) {
 				doubleParent = parent.parent;
 			}	
 		}
